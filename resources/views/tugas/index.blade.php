491750c9d94fb43cfa96de4714da07ab6ea30180
farreladitya <farelistihsanaditya@gmail.com>
2021-11-22 16:16:15 +0700

    tambah bootsrap

--- resources/views/tugas/index.blade.php
+++ resources/views/tugas/index.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 	<title>Tabel Tugas</title>
 </head>
 <body>
+    <div class="container">
     <h1>Tabel Tugas</h1>
-	<a href="/tugas/tambah"> + Tambah Tugas Baru</a>
+	<a href="/tugas/tambah"  class="btn btn-primary"> + Tambah Tugas Baru</a>
 
 	<br/>
 	<br/>
 
-	<table border="1">
+	<table border="1" class="table table-striped table-hover">
 		<tr>
             <th>ID</th>
 			<th>IDPegawai</th>
@@ -27,14 +35,14 @@
 			<td>{{ $p->NamaTugas }}</td>
 			<td>{{ $p->Status }}</td>
 			<td>
-				<a href="/tugas/edit/{{ $p->ID }}">Edit</a>
+				<a href="/tugas/edit/{{ $p->ID }}" class="btn btn-warning">Edit</a>
 				|
-				<a href="/tugas/hapus/{{ $p->ID}}">Hapus</a>
+				<a href="/tugas/hapus/{{ $p->ID}}" class="btn btn-danger">Hapus</a>
 			</td>
 		</tr>
 		@endforeach
 	</table>
-
-
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
