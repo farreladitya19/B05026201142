430e962aa049d13c5d8cc7dd32b96d1b1fcaad3a
farreladitya <farelistihsanaditya@gmail.com>
2021-12-14 00:01:11 +0700

    edit snack table

--- resources/views/pegawai/tambah.blade.php
+++ resources/views/pegawai/tambah.blade.php
@@ -1,16 +1,16 @@
 @extends("layout.ceria")
-@section("title", "Input Snack")
+@section("title", "Input Prgawai")
 
 @section("isikonten")
-    <h1>Input Snack</h1>
+    <h1>Input Pegawai</h1>
 
-	<a href="/snack " class="btn btn-secondary"> < Kembali</a>
+	<a href="/pegawai " class="btn btn-secondary"> < Kembali</a>
 
 	<br/>
 	<br/>
 
 
-	<form action="/snack/store" method="post">
+	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
         <div class="mb-3">
             <label class="form-label">Nama</label>
